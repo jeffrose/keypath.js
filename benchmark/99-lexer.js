e9f006c964067dc83227faca2585ef2efbd258ec
Aaron Brown <aaron@thebrownproject.com>
2016-10-17 15:23:06 +0000

    Added tk.getTokens to the lexer benchmark

--- benchmark/99-lexer.js
+++ benchmark/99-lexer.js
@@ -2,13 +2,14 @@
 
 var esprima = require( 'esprima' ),
     Lexer = require( '../dist/lexer-umd' ),
+    tk = require('../dist/tk-umd'),
     
     lexer = new Lexer(),
     
     dPath = 'foo.bar.qux.baz',
     bPath = '["foo"]["bar"]["qux"]["baz"]',
     pPath = 'foo(123)(456)(789)';
-
+    
 module.exports = {
     name: 'Lexing',
     maxTime: 5,
@@ -19,17 +20,26 @@ module.exports = {
         'esprima#tokenize-dot': function(){
             esprima.tokenize( dPath );
         },
+        'tk#getTokens-dot': function(){
+            tk.getTokens( dPath );
+        },
         'Lexer#lex-bracket': function(){
             lexer.lex( bPath );
         },
         'esprima#tokenize-bracket': function(){
             esprima.tokenize( bPath );
         },
+        'tk#getTokens-bracket': function(){
+            tk.getTokens( bPath );
+        },
         'Lexer#lex-paren': function(){
             lexer.lex( pPath );
         },
         'esprima#tokenize-paren': function(){
             esprima.tokenize( pPath );
+        },
+        'tk#getTokens-paren': function(){
+            tk.getTokens( pPath );
         }
     }
 };
