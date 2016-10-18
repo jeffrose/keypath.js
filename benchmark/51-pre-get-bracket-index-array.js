39a06488da5712ff0864a1db1a660662b0aade79
Jeff Rose <jefuri.rozu@gmail.com>
2016-10-18 00:51:07 +0000

    New benchmarks. Bug fix.

--- benchmark/51-pre-get-bracket-index-array.js
+++ benchmark/51-pre-get-bracket-index-array.js
@@ -3,27 +3,19 @@
 var KeyPathExp = require( '../dist/keypath-umd' ),
     tk = require( '../dist/tk-umd' ),
     
-    path = '["foo","bar"]["qux"]["baz"]',
-    data = {
-        foo: {
-            qux: {
-                baz: 123
-            }
-        },
-        bar: {
-            qux: {
-                baz: 456
-            }
-        }
-    },
-
+    path = '[1,2][1][0]',
+    data = [
+        [ [ 1 ], [ 2 ] ],// 0
+        [ [ 3 ], [ 4 ] ],// 1
+        [ [ 5 ], [ 6 ] ],// 2
+        [ [ 7 ], [ 8 ] ] // 3
+    ],
+    
     kpex = new KeyPathExp( path ),
     tkTokens = tk.getTokens( path );
-
-console.log( kpex.get( data ) );
     
 module.exports = {
-    name: 'Precompiled:Get:Bracket:Array',
+    name: 'Precompiled:Get:Bracket:Index:Array',
     maxTime: 5,
     tests: {
         'KeyPathExp#get': function(){
---
+++ benchmark/71-pre-get-bracket-prop-array.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var KeyPathExp = require( '../dist/keypath-umd' ),
+    tk = require( '../dist/tk-umd' ),
+    
+    path = '["foo","bar"]["qux"]["baz"]',
+    data = {
+        foo: {
+            qux: {
+                baz: 123
+            }
+        },
+        bar: {
+            qux: {
+                baz: 456
+            }
+        }
+    },
+
+    kpex = new KeyPathExp( path ),
+    tkTokens = tk.getTokens( path );
+
+module.exports = {
+    name: 'Precompiled:Get:Bracket:Property:Array',
+    maxTime: 5,
+    tests: {
+        'KeyPathExp#get': function(){
+            kpex.get( data );
+        }/*, Is this supported?
+        'tk#get-tokenized': function(){
+            tk.get( data, tkTokens );
+        }
+        */
+    }
+};
